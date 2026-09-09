ed9f599b61d64bba300c7af70c5a510be078080e
Helena Reichenvater <reichenvaterhelena@gmail.com>
2026-09-09 12:12:04 -0400

    Time: 723 ms (59.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+select
+signups.user_id,
+  round(
+        avg(
+            case
+                when confirmations.action = 'confirmed' then 1.00 else 0
+                end
+                ),
+                2)
+                as confirmation_rate
+        from
+            signups
+        left join
+            confirmations
+                on signups.user_id = confirmations.user_id
+        group by
+        signups.user_id
+        order by
+        confirmation_rate
+  ;
+  
