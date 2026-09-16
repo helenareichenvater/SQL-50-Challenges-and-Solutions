dfbe58f95a3b06e81a15ff005b89702e8ff59ab3
Helena Reichenvater <reichenvaterhelena@gmail.com>
2026-09-16 14:08:23 -0400

    Time: 867 ms (19.04%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -3,17 +3,12 @@ select
     m.employee_id,
     m.name,
     count(e.employee_id) as reports_count,
-        round(
-            avg(
-                e.age) 
-                     , 0)
-                            as average_age
+        round(avg(e.age), 0) as average_age
 from
     employees as e
 inner join
     employees as m
-        on
-            e.reports_to = m.employee_id
+        on e.reports_to = m.employee_id
 group by
     m.employee_id,
     m.name
